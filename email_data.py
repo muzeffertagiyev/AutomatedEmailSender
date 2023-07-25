7836caafc596e13ade51e2d792607f83bd2e2c2a
Muzeffer <75939608+muzeffertagiyev@users.noreply.github.com>
2023-07-25 11:27:51 +0200

    Update email_data.py

--- email_data.py
+++ email_data.py
@@ -5,7 +5,7 @@ import requests
 class EmailData:
   
     def __init__(self):
-        response = requests.get(url='https://api.npoint.io/de5c3533349a7f588f4d')
+        response = requests.get(url='https://api.npoint.io/de5d')
         self.email_data = response.json()
         self.emails = [email for index, email in enumerate(self.email_data['emails'])] 
         self.content = self.email_data['content']
